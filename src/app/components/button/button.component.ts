0a72cbf6039bbfa183d529662191a0c1e91cd792
Brezo <brezo.al@gmail.com>
2020-10-21 17:11:05 +0200

    Âdded styles and modifiers for buttons

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+
+  @Input() variant:string;
+  @Input() disabled: boolean = false;
+  @Input() text: string = "Default";
+  @Input() showIconBefore: boolean = false;
+  @Input() showIconAfter: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
